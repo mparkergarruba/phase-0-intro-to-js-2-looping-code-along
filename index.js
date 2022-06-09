b3211276bbc8b2db1aa67dfccadd26c1b7e087ec
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-06-08 20:46:56 -0400

    commit tree 85c19022e5f8757c2a6544a3aaad0bd318540d74

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const cardsToSend =[]
+
+function writeCards([...names], eventName) {
+    debugger
+    for (let i = 0; i < names.length; i++) {
+        cardsToSend.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cardsToSend
+}
+
+function countDown(number) {
+    let i = 0
+    while (number >= i) {
+        console.log(number)
+        number -= 1
+    }
+}
